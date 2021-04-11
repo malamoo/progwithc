cd1d2ea1d2ad849179f8d45a5b29856f8f337085
rfkm512 <rfkm512@protonmail.com>
2021-04-11 23:19:24 +1000

    Apply new coding style

--- ch5/amicable.c
+++ ch5/amicable.c
@@ -1,69 +1,40 @@
-/* Search for and print numbers that are amicable pairs.
-*/
+/* Search for and print numbers that are amicable pairs. */
 #include <stdio.h>
 
-int isamicable(int n, int m);
 int sum_proper_divisors(int n);
 
-int main(int argc, char const *argv[])
-{
-    int n, s;
-
-    for (n = 1;; n++)
-    {
-        s = sum_proper_divisors(n);
-
-        if (s <= n)
-        {
-            continue;
-        }
-
-        if (sum_proper_divisors(s) == n)
-        {
-            printf("%d, %d\n", n, s);
-        }
-    }
-
-    return 0;
-}
-
-/* Return true if two numbers are amicable.
-*/
-int isamicable(int n, int m)
-{
-    if (n == m)
-    {
-        return 0;
-    }
-
-    return sum_proper_divisors(n) == m &&
-           sum_proper_divisors(m) == n;
-}
-
-/* Return the sum of an integer's proper divisors.
-*/
+/* Return the sum of an integer's proper divisors. */
 int sum_proper_divisors(int n)
 {
-    int d, sum = 0;
+        int d;
+        int sum = 0;
 
-    if (n == 1)
-    {
+        if (n == 1)
+                return sum;
+
+        for (d = 1; d * d <= n; d++) {
+                if (n % d != 0)
+                        continue;
+                sum += d;
+                if (n / d != n && n / d != d)
+                        sum += n / d;
+        }
+
         return sum;
-    }
+}
 
-    for (d = 1; d * d <= n; d++)
-    {
-        if (n % d != 0)
-        {
-            continue;
+int main(int argc, char const **argv)
+{
+        int n;
+        int sum;
+
+        printf("Amicable pairs: \n");
+
+        for (n = 1;; n++) {
+                sum = sum_proper_divisors(n);
+                if (sum_proper_divisors(sum) == n)
+                        printf("(%d, %d)\n", n, sum);
         }
 
-        sum += d;
-        if (d != 1 && (d * d != n))
-        {
-            sum += n / d;
-        }
-    }
-
-    return sum;
+        return 0;
 }
